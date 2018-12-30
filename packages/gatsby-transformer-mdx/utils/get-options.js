08fb092be408c3f73686aca909d06c4cf2f15d5b
Karolis Šarapnickis <pastas.k@gmail.com>
2018-12-30 21:21:01 +0200

    feat: options validation via Joi schema

--- packages/gatsby-transformer-mdx/utils/get-options.js
+++ packages/gatsby-transformer-mdx/utils/get-options.js
@@ -1,6 +1,19 @@
 const path = require('path');
-
+const Joi = require('joi');
 const cleanInsertableCode = require('./clean-insertable-code');
+
+const OPTIONS_SCHEMA = Joi.object().keys({
+  loaders: Joi.object()
+    .keys({
+      js: Joi.func().optional(),
+      mdx: Joi.func().optional(),
+    })
+    .optional(),
+  pagesPath: Joi.string(),
+  globalImports: Joi.string().optional(),
+  defaultLayout: Joi.string().optional(),
+  plugins: Joi.any().optional(),
+});
 
 module.exports = (pluginOptions = {}) => {
   const options = Object.assign(
@@ -8,9 +21,13 @@ module.exports = (pluginOptions = {}) => {
     pluginOptions
   );
 
+  const { error } = Joi.validate(options, OPTIONS_SCHEMA);
+
+  if (error) throw new Error(`Invalid gatsby-transformer-mdx options - ${error.toString()}`);
+
   if (options.loaders == null) options.loaders = {};
-  if (typeof options.loaders.js !== 'function') options.loaders.js = value => value;
-  if (typeof options.loaders.mdx !== 'function') options.loaders.mdx = value => value;
+  if (options.loaders.js == null) options.loaders.js = value => value;
+  if (options.loaders.mdx == null) options.loaders.mdx = value => value;
 
   options.globalImports = cleanInsertableCode(options.globalImports);
 
