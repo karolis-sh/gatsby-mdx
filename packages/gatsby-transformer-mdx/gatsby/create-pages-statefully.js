5fbe8339e493323e761fc8758928618779adedc4
Karolis Šarapnickis <pastas.k@gmail.com>
2019-08-02 12:47:00 +0300

    fix(dependency): swap gatsby-plugin-page-creator with gatsby-page-utils

--- packages/gatsby-transformer-mdx/gatsby/create-pages-statefully.js
+++ packages/gatsby-transformer-mdx/gatsby/create-pages-statefully.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { promisify } = require('util');
 const globCB = require('glob');
-const chokidar = require('chokidar');
+const { watchDirectory } = require('gatsby-page-utils');
 
 const getOptions = require('../utils/get-options');
 const PageCreator = require('../utils/page-creator');
@@ -12,22 +12,17 @@ module.exports = async ({ store, actions }, pluginOptions) => {
   const { pagesPath } = getOptions(pluginOptions);
   const { createPage, deletePage } = actions;
 
-  const pagesDirectory = path.posix.join(pagesPath);
-  const pagesGlob = `${pagesDirectory}/**/*.mdx`;
+  const pagesDirectory = path.resolve(process.cwd(), pagesPath);
+  const pagesGlob = '**/*.mdx';
   const pageCreator = new PageCreator({ pagesDirectory, store, createPage, deletePage });
 
-  const files = await glob(pagesGlob);
+  const files = await glob(pagesGlob, { cwd: pagesPath });
   await Promise.all(files.map(file => pageCreator.create(file)));
 
-  await new Promise(resolve =>
-    chokidar
-      .watch(pagesGlob)
-      .on('add', async filePath => {
-        await pageCreator.create(filePath);
-      })
-      .on('unlink', filePath => {
-        pageCreator.remove(filePath);
-      })
-      .on('ready', resolve)
+  await watchDirectory(
+    pagesPath,
+    pagesGlob,
+    addedPath => pageCreator.create(addedPath),
+    removedPath => pageCreator.remove(removedPath)
   );
 };
