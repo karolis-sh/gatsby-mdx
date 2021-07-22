810f3be13bdd53b888ca04a2c2396502f0c4bc52
Karolis Šarapnickis <pastas.k@gmail.com>
2021-07-22 21:13:59 +0300

    chore: upgrade jest

--- packages/mdx-scoped-runtime/jest.config.js
+++ packages/mdx-scoped-runtime/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/__setup__.js'],
   snapshotSerializers: ['jest-serializer-html'],
   testRegex: 'src/.+\\.test\\.js$',
